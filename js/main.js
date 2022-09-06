291a8a064e8ea6059808abe85a4ae7132f0b3765
islomjon <ergashevislomjon1020@gmail.com>
2022-09-06 22:54:56 +0500

    Finish

--- js/main.js
+++ js/main.js
@@ -1,24 +1,53 @@
-var carOne = 0;
-var vertical = false;
-if (carOne >= 0 && carOne < 900) {
-    document.addEventListener('keydown', (event) => {
-        var name = event.key;
-        if (name == "ArrowRight") {
-            let car1 = document.querySelector(".carImg");
-            car1.style.transform = "translateX(" + carOne + "px)";
-            carOne += 10;
-        } else if (name == "ArrowLeft") {
-            let car1 = document.querySelector(".carImg");
-            car1.style.transform = "translateX(" + carOne + "px)";
-            carOne -= 10;
-        } else if (name == "ArrowUp" && vertical == true) {
-            let car1 = document.querySelector(".carImg");
-            car1.style.top = "65%";
-            vertical = false;
-        } else if (name == "ArrowDown" && vertical == false) {
-            let car1 = document.querySelector(".carImg");
-            car1.style.top = "70%";
-            vertical = true;
+function start() {
+    setInterval(function () {
+
+
+
+        var carOne = 0;
+        var vertical = false;
+        document.addEventListener('keydown', (event) => {
+            var name = event.key;
+            if (name == "ArrowRight") {
+                let car1 = document.querySelector(".carImg");
+                car1.style.transform = "translateX(" + carOne + "px)";
+                carOne += 10;
+                if (carOne == 1500) {
+                    var modal = document.querySelector(".modal");
+                    modal.style.display = "block";
+                    var text = document.querySelector(".text");
+                    text.textContent = "Siz yutdingiz!!!"
+                    return;
+                }
+            } else if (name == "ArrowLeft") {
+                let car1 = document.querySelector(".carImg");
+                car1.style.transform = "translateX(" + carOne + "px)";
+                carOne -= 10;
+            } else if (name == "ArrowUp" && vertical == true) {
+                let car1 = document.querySelector(".carImg");
+                car1.style.top = "65%";
+                vertical = false;
+            } else if (name == "ArrowDown" && vertical == false) {
+                let car1 = document.querySelector(".carImg");
+                car1.style.top = "70%";
+                vertical = true;
+            }
+        }, true);
+
+        let car2 = document.querySelector(".carImg2");
+        car2.style.transform = "translateX(1500px)";
+        function sayHi() {
+            var modal = document.querySelector(".modal");
+            modal.style.display = "block";
+            var text = document.querySelector(".text");
+            text.textContent = "Siz yutqazdingiz";
+            return;
         }
-    }, true);
+        setTimeout(sayHi, 10000);
+
+    }, 3000);
 }
+
+
+
+
+
